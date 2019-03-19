064f0837026c1c6e23df670b22d00d087f102c58
Thorben Bender <bender.thorben0@gmail.com>
2019-03-19 13:42:30 +0100

    get posts request done

--- Routes.js
+++ Routes.js
@@ -5,3 +5,26 @@ const db = require('./data/db');
 const routes = express.Router();
 
 routes.use(express.json());
+
+// routes.post('/api/posts', (req, res) => {
+
+// })
+
+routes.get('/api/posts', (req, res) => {
+	db
+		.find()
+		.then((data) => {
+			res.status(200).json(data);
+		})
+		.catch(() => {
+			res.status(500).json({ error: 'The posts information could not be retrieved.' });
+		});
+});
+
+// routes.get()
+
+// routes.delete();
+
+// routes.put();
+
+module.exports = routes;
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 
+const postRoutes = require('./Routes');
+
 const server = express();
 
 server.use(postRoutes);
