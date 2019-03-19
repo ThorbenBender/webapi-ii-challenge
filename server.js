812406f2f565edfd29f6fe06f215e33f08b50189
Thorben Bender <bender.thorben0@gmail.com>
2019-03-19 14:55:57 +0100

    working on stretch and added cors

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 
 const postRoutes = require('./Routes');
 
 const server = express();
 
 server.use(postRoutes);
+
+server.use(cors());
 
 module.exports = server;
