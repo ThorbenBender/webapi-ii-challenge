4e6328f6c35cfcde8db106b32b4aa57818840c02
Thorben Bender <bender.thorben0@gmail.com>
2019-03-19 13:51:42 +0100

    post request done

--- Routes.js
+++ Routes.js
@@ -6,9 +6,20 @@ const routes = express.Router();
 
 routes.use(express.json());
 
-// routes.post('/api/posts', (req, res) => {
-
-// })
+routes.post('/api/posts', (req, res) => {
+	if (!req.body.title || !req.body.title) {
+		res.status(400).json({ errorMessage: 'Please provide title and contents for the post.' });
+	} else {
+		db
+			.insert(req.body)
+			.then((data) => {
+				res.status(201).json(data);
+			})
+			.catch(() => {
+				res.status(500).json({ error: 'There was an error while saving the post to the database' });
+			});
+	}
+});
 
 routes.get('/api/posts', (req, res) => {
 	db
